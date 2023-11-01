0419120537948c9cb9ac9267751822b73568f801
Vikash Kumar <67106713+vikashkr99@users.noreply.github.com>
2023-11-01 10:43:56 +0530

    Update operation_eradication.py

--- operation_eradication.py
+++ operation_eradication.py
@@ -1,1 +1,12 @@
 # operation_eradication
+# Make sure to copy and paste the results of `rclone ls "Eradication:"` inside a text file names locations.txt.
+# i will update the script to do all that by itself.
+import os
+
+f = open('locations.txt', 'r')
+for i in f:
+    fine_name = i.split()[1].split('/')[-1]
+    file_location = i.split()[1][:-len(file_name)]
+    os.system(f'touch {file_name}')
+    os.system(f'rclone copy {file_name} "Eradication:/{file_location}"')
+    os.system(f'rm {file_name}')
